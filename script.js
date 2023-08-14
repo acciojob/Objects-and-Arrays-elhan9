fea5538073f2de65d3434cc6a3caaf1cf7f059d0
elhan9 <118253118+elhan9@users.noreply.github.com>
2023-08-14 16:52:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ const person = {
   age: 80,
 };
 
+const team = players;
+
+    // Task 2: Create a deep copy of the array players
+    const team1 = players.slice();
+
+    // Task 3: Create a deep copy of the object person
+    const cap1 = JSON.parse(JSON.stringify(person));
+
