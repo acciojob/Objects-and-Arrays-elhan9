7ec6445bded8f16cd9d51140272b026d974907ee
elhan9 <118253118+elhan9@users.noreply.github.com>
2023-08-14 16:50:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,11 +4,4 @@ const person = {
   name: "John Doe",
   age: 80,
 };
-const team = players;
-
-    // Task 2: Create a copy of the array players
-    const team1 = players.slice();
-
-    // Task 3: Create a copy of the object person
-    const cap1 = Object.assign({}, person);
 
