fae335172ea28bfcdfc5f4c6cd63b632fb5a51a0
elhan9 <118253118+elhan9@users.noreply.github.com>
2023-08-14 17:00:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ const person = {
   name: "John Doe",
   age: 80,
 };
+
+ const team = players;
+
+    // Task 2: Create a deep copy of the array players
+    const team1 = deepCopy(players);
+
+    // Task 3: Create a deep copy of the object person
+    const cap1 = deepCopy(person);
